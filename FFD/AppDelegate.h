a977077011eb32f3308e485b3322da8700460ec5
Thidaporn Kijkamjai <jinglejill@thidaporns-macbook-pro.local>
2017-09-17 14:22:42 +0700

    Initial Commit

---
+++ FFD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFD
+//
+//  Created by Thidaporn Kijkamjai on 9/17/2560 BE.
+//  Copyright © 2560 Appxelent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFD
+//
+//  Created by Thidaporn Kijkamjai on 9/17/2560 BE.
+//  Copyright © 2560 Appxelent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
